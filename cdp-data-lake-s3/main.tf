a29bffdfcc2efd85084b27249874d0bf4ce6735b
Saddam <67977757+saddamkafil@users.noreply.github.com>
2024-05-08 13:37:11 +1000

    Create main.tf

---
+++ cdp-data-lake-s3/main.tf
@@ -0,0 +1,24 @@
+module "cdp-data-lake" {
+    source = "../modules/s3"
+    bucket_name = "saddam-modulet-test-s3-bucket"
+    tag_name = "test"
+    environment_tag = "env-test"
+    versioning_status = "Disabled"
+    object_ownership = "BucketOwnerPreferred"
+    acl = "private"
+    create_website_configuration = false
+    index_document = "index.html"
+    error_document = "error.html"
+    sse_algorithm = "aws:kms"
+    block_public_acls = "true"
+    block_public_policy = "true"
+    ignore_public_acls = "true"
+    create_bucket_policy = false
+    policy = "na"
+    managed_by_tag = "managed-tag"
+    restrict_public_buckets = "true"
+    rule_id = "delete-incomplete-multipart-uploads-7days"
+    lifiecycle_rule_status = "Disabled"
+    bucket_lifecycle_configuration = true
+    delete_days_after_inititation = "7"
+}
